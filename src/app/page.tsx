c34ddfa3a7a131cbb17fb65b50dde8f3fc7f942f
Al3xand3r11 <alexander.nance11@gmail.com>
2023-06-28 16:44:47 -0700

    beginning work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,52 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useState } from 'react';
+
+type ContactFields = {
+  [key: string]: string|undefined;
+  fullname: string|undefined;
+  email: string|undefined;
+
+}
+
+type ErrorField = {
+  fullname: boolean;
+  email: boolean;
+}
 
 export default function Home() {
+
+  const initialContactState: ContactFields = {
+    fullname: "",
+    email: "",
+  }
+
+  const initialErrorState: ErrorField = {
+    fullname: false,
+    email: false,
+  }
+
+  const [dataSend, setDataSend] = useState<ContactFields>(initialContactState);
+  const [error, setError] = useState<ErrorField>(initialErrorState);
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div>
+        <form>
+        <label>Full Name</label>
+        <input type="text" value={dataSend.fullname ?? ""}
+        onBlur={() => setError({...error, fullname: true})}
+        onChange={(e) => setDataSend({...dataSend,fullname: e.target.value})}
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {error.fullname && !dataSend.fullname && <span>Please write your full name</span>}
+        <label>Email</label>
+        <input type="text" value={dataSend.email ?? ""}
+        onBlur={() => setError({...error, email: true})}
+        onChange={(e) => setDataSend({...dataSend,email: e.target.value})}/>
+        {error.email && (!dataSend.email || !/\S+@\S+\S+\.\S+/.test(dataSend.email)) && <span>Please write a valid email</span>}
+        <button type="submit">
+          Submit
+        </button>
+        </form>
       </div>
     </main>
   )
